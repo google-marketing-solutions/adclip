fca718f489d4a05df0193ced96c2fedbcf1e251c
Ryan Sibbaluca <ryansibbaluca@google.com>
2023-11-03 13:31:40 +0800

    Initialize frontend code with create-next-app and with-firebase template
    Change-Id: I470e1065ba042b7eba4ac0d19e542d338938d242

---
+++ frontend/pages/_app.js
@@ -0,0 +1,11 @@
+import UserProvider from '../context/userContext'
+import './global.sass'
+
+// Custom App to wrap it with context provider
+export default function App({ Component, pageProps }) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  )
+}
---
+++ frontend/pages/index.js
@@ -0,0 +1,31 @@
+import { getFirestore, setDoc, doc } from 'firebase/firestore'
+import Head from 'next/head'
+import { useEffect } from 'react'
+import { useUser } from '../context/userContext'
+import styles from './index.module.sass'
+
+export default function Home() {
+  // Our custom hook to get context values
+  const { loadingUser, user } = useUser()
+
+  useEffect(() => {
+    if (!loadingUser) {
+      // You know that the user is loaded: either logged in or out!
+      console.log(user)
+    }
+    // You also have your firebase app initialized
+  }, [loadingUser, user])
+
+  return (
+    <div>
+      <Head>
+        <title>AdClip</title>
+        <link rel="icon" href="/adclip.ico" />
+      </Head>
+
+      <main>
+        <h1>AdClip</h1>
+      </main>
+    </div>
+  )
+}

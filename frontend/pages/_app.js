8cb30c9379128ce7bd7bd993997a8db1b9165250
Ryan Sibbaluca <ryansibbaluca@google.com>
2023-11-03 18:37:02 +0800

    Redirect signed out users to signIn page
    Change-Id: I25033d4b4ffc96b32affb84519db9520451ec0d7

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,36 @@
+import {useEffect} from 'react';
 import UserProvider from '../context/userContext';
+import {useUser} from '../context/userContext';
 import './global.sass';
+import {usePathname, useRouter} from 'next/navigation';
+
+function Layout({children}) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const {loadingUser, user} = useUser();
+
+  useEffect(() => {
+    if (loadingUser) return;
+
+    // if logged in, redirect to home screen
+    if (user && pathname === '/sign-in') {
+      router.push('/');
+    }
+    if (!user && pathname !== '/sign-in') {
+      router.push('/signIn');
+    }
+  }, [pathname, user, router, loadingUser]);
+
+  return children;
+}
 
 // Custom App to wrap it with context provider
 export default function App({Component, pageProps}) {
   return (
     <UserProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </UserProvider>
   );
 }

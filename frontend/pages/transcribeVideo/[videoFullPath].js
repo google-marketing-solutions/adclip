ae8e06a90e8fe9745b194d4e94a4cf2a059f5499
Ryan Sibbaluca <ryansibbaluca@google.com>
2023-11-08 16:13:10 +0800

    Use video name as title in transcript review page
    Change-Id: I29ddadb0d6cb28ee8ad60b5128083644bc3b8b88

--- frontend/pages/transcribeVideo/[videoFullPath].js
+++ frontend/pages/transcribeVideo/[videoFullPath].js
@@ -1,10 +1,17 @@
 import {useRef} from 'react';
 import styles from './index.module.sass';
+import Head from 'next/head';
 import {useRouter} from 'next/router';
 import TranscriptsContainer from './TranscriptsContainer';
 import VideoReference from '../../components/VideoReference';
 import DurationInput from '../../components/DurationInput';
 import Store from '../../store/AdClipStore';
+
+const getFilenameFromFullPath = (fullPath) => {
+  if (fullPath == null) return null;
+  const filePathArr = fullPath.split('/');
+  return filePathArr[filePathArr.length - 1];
+};
 
 function TranscriptReview() {
   const store = Store.useStore();
@@ -13,31 +20,39 @@ function TranscriptReview() {
   const isTranscribingVideo = store.get('isTranscribingVideo');
   const playerRef = useRef(null);
 
+  const filename = getFilenameFromFullPath(videoFullPath);
+
   return (
-    <div className={styles.transcriptReviewContainer}>
-      <h2>Review Video Transcription</h2>
-      <h3>{videoFullPath}</h3>
-      <p>
-        Please review and edit the video transcript as needed to ensure
-        accuracy.
-      </p>
-      <main>
-        <div>
-          <TranscriptsContainer playerRef={playerRef} />
-          <br />
-        </div>
-        <div>
-          <p>Video Reference</p>
-          <VideoReference ref={playerRef} />
-          <div className={styles.nextButtonContainer}>
-            <DurationInput
-              disabled={isTranscribingVideo}
-              submitText="Summarize Transcript"
-            />
+    <>
+      <Head>
+        <title>{filename}</title>
+      </Head>
+
+      <div className={styles.transcriptReviewContainer}>
+        <h2>Review Video Transcription</h2>
+        <h3>{filename}</h3>
+        <p>
+          Please review and edit the video transcript as needed to ensure
+          accuracy.
+        </p>
+        <main>
+          <div>
+            <TranscriptsContainer playerRef={playerRef} />
+            <br />
           </div>
-        </div>
-      </main>
-    </div>
+          <div>
+            <p>Video Reference</p>
+            <VideoReference ref={playerRef} />
+            <div className={styles.nextButtonContainer}>
+              <DurationInput
+                disabled={isTranscribingVideo}
+                submitText="Summarize Transcript"
+              />
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
 

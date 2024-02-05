dce3821159404463f8c7cebce4367db0bc08de81
Lan Tran <lantrann@google.com>
2024-02-05 04:51:58 +0000

    add other languages to transcribe function, format some lines
    Change-Id: I1c7672a2104e0a39e3c6b147c54d896b9e2e5faf

--- functions/transcribe_video/main.py
+++ functions/transcribe_video/main.py
@@ -11,16 +11,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from google.cloud import speech, storage
-import moviepy.editor as mpy
+
 import itertools
-
-from firebase_functions import https_fn, options
-from firebase_admin import initialize_app, firestore
-
+from firebase_admin import firestore
+from firebase_admin import initialize_app
+from firebase_functions import https_fn
+from firebase_functions import options
+from google.cloud import speech
+from google.cloud import storage
+import moviepy.editor as mpy
 from video_intelligence import process_video
 
+
 initialize_app()
+
 
 GCLOUD_BUCKET_NAME = 'adclip.appspot.com'
 AUDIO_FOLDER = 'videos/audio/'
@@ -28,32 +32,43 @@ TEMP_FOLDER = '/tmp/'
 GS_PATH = f'gs://{GCLOUD_BUCKET_NAME}/'
 GAP_MULTIPLIER = 2.5
 MIN_CLIP_DURATION = 5
+LANGUAGE_CODE = 'en-US'
+
 
 storage_client = storage.Client()
 bucket = storage_client.get_bucket(GCLOUD_BUCKET_NAME)
 
-def get_speech_recognition_config(language_code: str, model: str):
+
+def get_speech_recognition_config(language_code: str):
   """Get speech recognition config from the given language code and model.
+
   Check all supported language code and model here:
   https://cloud.google.com/speech-to-text/docs/speech-to-text-supported-languages
 
   Args:
-    language_code: A langauge code for transribing.
+    language_code: A language code for transribing.
     model: A video transcribe model.
 
   Return:
     A speech recognition config.
   """
+  if language_code == 'en-US':
+    model = 'video'
+  elif language_code == 'zh-TW':
+    model = 'command_and_search'
+  else:
+    model = 'default'
   return speech.RecognitionConfig(
-    enable_word_time_offsets=True,
-    audio_channel_count=2, #2 is default for wav files
-    # Enable automatic punctuation
-    # enable_automatic_punctuation=True,
-    language_code=language_code,
-    # alternative_language_codes=alternate_languages,
-    model=model,
-    # Works only for model="video" or "phone call"
-    use_enhanced=True)
+      enable_word_time_offsets=True,
+      audio_channel_count=2,  # 2 is default for wav files
+      # Enable automatic punctuation
+      # enable_automatic_punctuation=True,
+      language_code=language_code,
+      model=model,
+      # Works for model="video" or "phone call" (en-US only)
+      use_enhanced=True,
+  )
+
 
 def does_file_exist(file_path: str) -> bool:
   """Validate if file already existing in the bucket.
@@ -61,20 +76,24 @@ def does_file_exist(file_path: str) -> bool:
   Args:
     file_path: A file location.
 
-  Return:
+  Returns:
     True if file existed, otherwise, False
   """
   blob = bucket.get_blob(file_path)
   return blob is not None
+
 
 def upload_blob(source_file_name: str, destination_blob_name: str) -> None:
   """Upload file to bucket."""
   blob = bucket.blob(destination_blob_name)
   blob.upload_from_filename(source_file_name)
 
-  print('File {} uploaded to {}.'.format(source_file_name, destination_blob_name))
+  print(
+      'File {} uploaded to {}.'.format(source_file_name, destination_blob_name)
+  )
 
-def extract_audio(video_full_path, file_name, output_name = None) -> str:
+
+def extract_audio(video_full_path, file_name, output_name=None) -> str:
   """Extract audio from the video by the given video path.
 
   Args:
@@ -108,13 +127,14 @@ def extract_audio(video_full_path, file_name, output_name = None) -> str:
 
   return GS_PATH + gcs_file_path
 
+
 def build_transcript(response) -> list:
   """Build video transcript response with transcript metadata.
 
   Args:
     response: A transcript response from speech API.
 
-  Return:
+  Returns:
     A list of new video transcript strucutre and metadata.
     For example,
     [
@@ -150,38 +170,40 @@ def build_transcript(response) -> list:
 
       if len(alternative.words) > 0:
         transcript_item = {
-          'text': alternative.transcript,
-          'startTime': alternative.words[0].start_time.total_seconds(),
-          'endTime': alternative.words[-1].end_time.total_seconds(),
-          'duration': (alternative.words[-1].end_time.total_seconds()
-                      - alternative.words[0].start_time.total_seconds())
+            'text': alternative.transcript,
+            'startTime': alternative.words[0].start_time.total_seconds(),
+            'endTime': alternative.words[-1].end_time.total_seconds(),
+            'duration': (alternative.words[-1].end_time.total_seconds()
+                         - alternative.words[0].start_time.total_seconds())
         }
 
         transcript_item['words'] = []
         for word in alternative.words:
           transcript_item['words'].append({
-            'text': word.word,
-            'startTime': word.start_time.total_seconds(),
-            'endTime': word.end_time.total_seconds(),
-            'duration': (word.end_time.total_seconds()
-                        - word.start_time.total_seconds()),
-            'gap': word.end_time.total_seconds() - last_end_time})
+              'text': word.word,
+              'startTime': word.start_time.total_seconds(),
+              'endTime': word.end_time.total_seconds(),
+              'duration': (word.end_time.total_seconds()
+                           - word.start_time.total_seconds()),
+              'gap': word.end_time.total_seconds() - last_end_time})
           last_end_time = word.end_time.total_seconds()
         transcript_builder.append(transcript_item)
   return transcript_builder
 
+
 def generate_transcript_item(
     words: list, start_time: float = None, end_time: float = None) -> dict:
-  """Generate transcript item."""
+  """Generates transcript item."""
   start_time = words[0]['startTime'] if start_time is None else start_time
   end_time = words[-1]['endTime'] if end_time is None else end_time
   return {
-    'text': ' '.join(list(map(lambda word: word['text'], words))),
-    'startTime': start_time,
-    'endTime': end_time,
-    'duration': end_time - start_time,
-    'words': words
+      'text': ' '.join(list(map(lambda word: word['text'], words))),
+      'startTime': start_time,
+      'endTime': end_time,
+      'duration': end_time - start_time,
+      'words': words
   }
+
 
 def refine_by_gaps(transcript: list) -> list:
   """Refines the transcript by the gap time."""
@@ -204,11 +226,13 @@ def refine_by_gaps(transcript: list) -> list:
       new_transcript.append(generate_transcript_item(words))
   return new_transcript
 
+
 def upload_video_shots(file_name: str, video_shots: list) -> None:
   """Uploads video shot to firestore."""
   db = firestore.client()
   doc_ref = db.collection('video_shots').document(file_name)
   doc_ref.set({'data': video_shots})
+
 
 def get_video_shots(file_name: str) -> bool:
   """Gets video shots from firestore by file name."""
@@ -217,6 +241,7 @@ def get_video_shots(file_name: str) -> bool:
   if not doc.exists:
     return None
   return doc.to_dict().get('data')
+
 
 def merge_clips(transcript: list) -> list:
   """Merges clips under 5seconds."""
@@ -228,12 +253,12 @@ def merge_clips(transcript: list) -> list:
     start_time = transcript1['startTime']
     end_time = max(transcript1['endTime'], transcript2['endTime'])
     return {
-      'text': f"{transcript1['text']} {transcript2['text']}",
-      'startTime': start_time,
-      'endTime': end_time,
-      'duration': end_time - start_time,
-      'words': transcript1['words'] + transcript2['words']
-      }
+        'text': f"{transcript1['text']} {transcript2['text']}",
+        'startTime': start_time,
+        'endTime': end_time,
+        'duration': end_time - start_time,
+        'words': transcript1['words'] + transcript2['words'],
+    }
 
   def is_overlapping(transcript1, transcript2):
     """Validate overlapping transcript time."""
@@ -241,8 +266,7 @@ def merge_clips(transcript: list) -> list:
     t2_prev_start_time = transcript2['words'][-1]['startTime']
     t1_start_time = transcript1['startTime']
     t1_end_time = transcript1['endTime']
-    return (t2_start_time >= t1_start_time and
-      t2_prev_start_time <= t1_end_time)
+    return t2_start_time >= t1_start_time and t2_prev_start_time <= t1_end_time
 
   output = []
   index = 0
@@ -260,6 +284,7 @@ def merge_clips(transcript: list) -> list:
     else:
       output.append(clip)
   return output
+
 
 def refine_by_video_shots(
     file_name: str, video_gcs_uri: str, transcript: list) -> list:
@@ -288,83 +313,87 @@ def refine_by_video_shots(
       start_time = min(words[0]['startTime'], video_shot['start_time'])
       if index < len(transcript_words) - 1:
         end_time = max(
-          word['endTime'],
-          min(video_shot['end_time'],
-          transcript_words[index+1]['startTime']))
+            word['endTime'],
+            min(
+                video_shot['end_time'], transcript_words[index + 1]['startTime']
+            ),
+        )
       else:
         end_time = max(word['endTime'], video_shot['end_time'])
       video_shots_index = video_shots_index + 1
       new_transcript.append(
-        generate_transcript_item(words, start_time, end_time))
+          generate_transcript_item(words, start_time, end_time)
+      )
       words = []
   if len(words) > 0:
     start_time = min(
-      words[0]['startTime'],
-      video_shots[video_shots_index]['start_time'])
-    if (len(new_transcript) > 0):
+        words[0]['startTime'], video_shots[video_shots_index]['start_time']
+    )
+    if len(new_transcript) > 0:
       previous_last_word = new_transcript[-1]['words'][-1]
       start_time = max(start_time, previous_last_word['endTime'])
 
-    end_time = max(
-      word['endTime'],
-      video_shots[video_shots_index]['end_time'])
+    end_time = max(word['endTime'], video_shots[video_shots_index]['end_time'])
     video_shots_index = video_shots_index + 1
-    new_transcript.append(
-      generate_transcript_item(words, start_time, end_time))
+    new_transcript.append(generate_transcript_item(words, start_time, end_time))
 
   return new_transcript
 
+
 def get_transcript(file_name: str) -> bool:
-  """Get transcript from firestore by file name."""
+  """Gets transcript from firestore by file name."""
   db = firestore.client()
   doc = db.collection('transcripts').document(file_name).get()
   if not doc.exists:
-      return None
+    return None
 
   return doc.to_dict().get('original')
+
 
 def upload_transcript(file_name: str, transcript: list) -> None:
   db = firestore.client()
   doc_ref = db.collection('transcripts').document(file_name)
-  doc_ref.set({"original": transcript})
+  doc_ref.set({'original': transcript})
 
-@https_fn.on_call(timeout_sec=600, memory=options.MemoryOption.GB_4, cpu=2,
-  region='asia-southeast1')
+
+@https_fn.on_call(
+    timeout_sec=600,
+    memory=options.MemoryOption.GB_4,
+    cpu=2,
+    region='asia-southeast1',
+)
 def transcribe_video(request: https_fn.CallableRequest) -> any:
-  """Transcribe video audio and store the transcript in GCS.
+  """Transcribes video audio and store the transcript in GCS.
 
   Args:
     request: A request payload from API call.
 
-  Return:
+  Returns:
     An object that contain video transcript with the timestamp data.
   """
 
   video_full_path = request.data['full_path']
   file_name = request.data['file_name']
-
-  try:
-    language_code = request.data['language_code']
-    model = request.data['model']
-  except:
-    language_code = 'en-US'
-    model = 'video'
+  language_code = request.data['language_code'] or LANGUAGE_CODE
 
   if video_full_path is None:
     return {
-      'error': 'Missing video uri, sample format: https://googleapis.com/Welcome to World Class.wav'
+        'error': (
+            'Missing video uri, sample format:'
+            ' https://googleapis.com/example.wav'
+        )
     }
 
   transcript_in_firestore = get_transcript(file_name)
   if transcript_in_firestore is not None:
     return {
-      'transcript': merge_clips(
-        refine_by_video_shots(
-          file_name,
-          GS_PATH + video_full_path,
-          transcript_in_firestore)),
-      'original': transcript_in_firestore,
-      'v1': refine_by_gaps(transcript_in_firestore),
+        'transcript': merge_clips(
+            refine_by_video_shots(
+                file_name,
+                GS_PATH + video_full_path,
+                transcript_in_firestore)),
+        'original': transcript_in_firestore,
+        'v1': refine_by_gaps(transcript_in_firestore),
     }
 
   audio_gcs_uri = extract_audio(video_full_path, file_name)
@@ -373,8 +402,7 @@ def transcribe_video(request: https_fn.CallableRequest) -> any:
   audio = speech.RecognitionAudio(uri=audio_gcs_uri)
   client = speech.SpeechClient()
 
-  #TODO: b/306533157 - Get language model and video transcribe model from request payload.
-  config = get_speech_recognition_config(language_code, model)
+  config = get_speech_recognition_config(language_code)
 
   operation = client.long_running_recognize(config=config, audio=audio)
 
@@ -385,11 +413,11 @@ def transcribe_video(request: https_fn.CallableRequest) -> any:
   upload_transcript(file_name, transcript)
 
   return {
-    'transcript': merge_clips(
-      refine_by_video_shots(
-        file_name,
-        GS_PATH + video_full_path,
-        transcript)),
-    'original': transcript,
-    'v1': refine_by_gaps(transcript)
+      'transcript': merge_clips(
+          refine_by_video_shots(
+              file_name,
+              GS_PATH + video_full_path,
+              transcript)),
+      'original': transcript,
+      'v1': refine_by_gaps(transcript)
   }
